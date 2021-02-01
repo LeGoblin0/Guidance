f99703ffbc5a43c215f821a59755dfbda6eba225
LeGoblin0 <legoblin@naver.com>
2021-02-02 04:40:37 +0900

    적이 아기에게 따라오는 AI 작업
    적이 아기에게 따라오는 AI 작업

---
+++ Assets/02. Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public float EnemySpeed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        if(Vector2.Distance(transform.position, target.position) > 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, EnemySpeed * Time.deltaTime);
+        }
+    }
+}
+  
